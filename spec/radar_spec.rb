78f5e2cd9cb030c705884034ec2506db802e0bef
skyfx <chielkas@gmail.com>
2017-04-16 22:42:58 +0200

    Update radar_spec.rb

--- spec/radar_spec.rb
+++ spec/radar_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Radar, ".status_for" do
     end
 
     it "has a city" do
-      expect(status.city).to eq("New York")
+      expect(status.city).to eq("New York city")
     end
 
     it "has weather" do
